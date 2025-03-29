56f49802334509a5ce692dab34fb1a4b27e4f80a
gettygetty08 <gettygetty08@gmail.com>
2025-03-29 21:40:49 +0900

    拡張メソッドの適応

--- DDD.Domain/Helpers/FloatHelper.cs
+++ DDD.Domain/Helpers/FloatHelper.cs
@@ -2,9 +2,12 @@
 
 namespace DDD.Domain.Helpers
 {
+    /// <summary>
+    /// 小数点以下を指定桁数で四捨五入します
+    /// </summary>
     public static class FloatHelper
     {
-        public static string RoundString(float value, int decimalPoint)
+        public static string RoundString(this float value, int decimalPoint)
         {
             var temp = Convert.ToSingle(Math.Round(value, decimalPoint));
             return temp.ToString("F" + decimalPoint);
